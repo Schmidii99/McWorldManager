a133647a01a285659fc3b02c533a2f4c78812cfa
Florian Schmid <Melonencool@gmail.com>
2024-07-09 16:01:11 +0200

    get world from default save folder

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,13 +4,23 @@
 mod database;
 mod state;
 
+use std::default;
+use std::fs;
+use std::fs::DirEntry;
+use std::path::PathBuf;
+use base64::{engine::general_purpose, Engine as _};
+
 use state::{AppState, ServiceAccess};
+use tauri::api::path;
 use tauri::{State, Manager, AppHandle};
+
+use rust_search::{SearchBuilder, similarity_sort};
+use platform_dirs::AppDirs;
 
 fn main() {
   tauri::Builder::default()
       .manage(AppState { db: Default::default() })
-      .invoke_handler(tauri::generate_handler![greet])
+      .invoke_handler(tauri::generate_handler![greet, get_saves])
       .setup(|app| {
           let handle = app.handle();
 
@@ -34,4 +44,28 @@ fn greet(app_handle: AppHandle, name: &str) -> String {
     let items_string = items.join(" | ");
 
     format!("Your name log: {}", items_string)
+}
+
+#[tauri::command]
+fn get_saves(_app_handle: AppHandle) -> String {
+    let minecraft_folder_path: PathBuf = AppDirs::new(Some(".minecraft\\saves"), true).unwrap().config_dir;
+
+    let paths = fs::read_dir(minecraft_folder_path).unwrap();
+
+    let mut saves: String = String::from("");
+
+    for p in paths {
+        if p.as_ref().unwrap().metadata().unwrap().is_dir() {
+
+            let region_path = p.as_ref().unwrap().path().join("region");
+            // println!("{}", region_path.display());
+
+            // check if region dir exists
+            if region_path.exists() {
+                saves += &(general_purpose::STANDARD.encode(p.as_ref().unwrap().path().display().to_string()) + ",");
+            }
+        }    
+    }
+
+    saves
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,23 @@
-<script>
-    import Greet from '../lib/Greet.svelte'
+<script lang="ts">
+    import WorldDisplay from '$lib/WorldDisplay.svelte';
+import { invoke } from '@tauri-apps/api/tauri';
+    import { onMount } from 'svelte';
+
+    let world_urls: string[] = [];
+
+    onMount(async () => {
+        let response: string = await invoke('get_saves');
+        // remove last empty item
+        world_urls = response.split(',').slice(0, -1);
+        
+        for (let i = 0; i < world_urls.length; i++) {
+            world_urls[i] = atob(world_urls[i]);
+        }
+    })
 </script>
-  
-<div class="flex justify-center">
-    <h1 class="text-xl underline">Welcome to SvelteKit</h1>
+
+<div class="flex flex-wrap">
+    {#each world_urls as url}
+        <WorldDisplay path={url} />
+    {/each}
 </div>
-<Greet />
