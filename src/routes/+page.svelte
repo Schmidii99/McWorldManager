230e4429b22824f593d31cf2666d2da38136d0be
Florian Schmid <Melonencool@gmail.com>
2024-07-11 22:33:20 +0200

    refactor to work with db data

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,23 +1,24 @@
 <script lang="ts">
     import WorldDisplay from '$lib/WorldDisplay.svelte';
-import { invoke } from '@tauri-apps/api/tauri';
+    import { invoke } from '@tauri-apps/api/tauri';
     import { onMount } from 'svelte';
 
-    let world_urls: string[] = [];
+    let world_paths: string[] = [];
 
     onMount(async () => {
-        let response: string = await invoke('get_saves');
+        let response: string = await invoke('get_saved_paths');
         // remove last empty item
-        world_urls = response.split(',').slice(0, -1);
+        console.log(response);
+        world_paths = response.split(',');
         
-        for (let i = 0; i < world_urls.length; i++) {
-            world_urls[i] = atob(world_urls[i]);
+        for (let i = 0; i < world_paths.length; i++) {
+            world_paths[i] = atob(world_paths[i]);
         }
     })
 </script>
 
 <div class="flex flex-wrap">
-    {#each world_urls as url}
-        <WorldDisplay path={url} />
+    {#each world_paths as path}
+        <WorldDisplay path={path} />
     {/each}
 </div>
