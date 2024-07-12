39a5af0cdf616a81956cc837eee3f448354514c0
Florian Schmid <Melonencool@gmail.com>
2024-07-12 15:12:11 +0200

    react to messagebox and refresh from backend

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,27 @@
-<script>
+<script lang="ts">
+    import { onMount } from "svelte";
     import "../app.css";
+    import { listen } from '@tauri-apps/api/event';
+    import { message } from '@tauri-apps/api/dialog';
+    
+    let unlistenMessageboxError: null | any = null;
+    let unlistenMessagebox: null | any = null;
+
+    onMount(async () => {
+        if (unlistenMessageboxError == null) {
+            unlistenMessageboxError = await listen('messagebox_error', async (event: any) => {
+                await message(event.payload["message"], { title: event.payload["title"], type: 'error' });
+            });
+        }
+        if (unlistenMessagebox == null) {
+            unlistenMessagebox = await listen('messagebox', async (event: any) => {
+                console.log("Showing messagebox")
+                await message(event.payload["message"], { title: event.payload["title"], type: 'info' });
+            });
+        }
+        
+    });
+
 </script>
   
 <slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,19 +2,29 @@
     import WorldDisplay from '$lib/WorldDisplay.svelte';
     import { invoke } from '@tauri-apps/api/tauri';
     import { onMount } from 'svelte';
-    import { atobUTF8 } from '$lib/helper';
+    import { atobUTF8 } from '$lib/helper'
+    import { listen } from '@tauri-apps/api/event';
 
     let world_paths: string[] = [];
 
+    let unlistenRefresh = null;
+
     onMount(async () => {
+        await loadWorlds();
+
+        unlistenRefresh = await listen('refresh', (event) => {
+            loadWorlds();
+        })
+    })
+
+    async function loadWorlds() {
         let response: string = await invoke('get_saved_paths');
-        // remove last empty item
         world_paths = response.split(',');
         
         for (let i = 0; i < world_paths.length; i++) {
             world_paths[i] = atobUTF8(world_paths[i]);
         }
-    })
+    }
 </script>
 
 <div class="flex flex-wrap">
