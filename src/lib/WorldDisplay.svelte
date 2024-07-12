bfe11d275d3bfacd33b1c3dbd923b2408d3aff5f
Florian Schmid <Melonencool@gmail.com>
2024-07-12 10:30:54 +0200

    check if img exists

--- src/lib/WorldDisplay.svelte
+++ src/lib/WorldDisplay.svelte
@@ -1,18 +1,20 @@
 <script lang="ts">
     import { onMount } from "svelte";
     import { convertFileSrc, invoke } from '@tauri-apps/api/tauri';
-    import { readDir } from '@tauri-apps/api/fs';
+    import { readDir, exists } from '@tauri-apps/api/fs';
     import { getVersionName } from "./MinecraftVersions";
 
     export let path: string;
 
-    let img_url = "";
+    let img_url: string | null = null;
     let playerdata: any | null = null;
 
     onMount(async () => {
-        img_url = convertFileSrc(path + "/icon.png");
+        if (await exists(path + "\\icon.png")) {
+            img_url = convertFileSrc(path + "/icon.png");
+        }
 
-        const firstPlayerDataFileName = (await readDir(path + "/playerdata/"))[0].name;
+        const firstPlayerDataFileName = (await readDir(path + "\\playerdata\\"))[0].name;
         
         let contents: string = await invoke("deserialize_nbt_file", {path: path + "\\playerdata\\" + firstPlayerDataFileName});
         
@@ -21,7 +23,9 @@
 </script>
 
 <div class="flex flex-col min-w-48 min-h-64 max-w-48 bg-teal-300 ml-4 mt-4 overflow-hidden">
-    <img class="w-48 h-48" src="{img_url}" alt="Thumbnail">
+    {#if img_url}
+        <img class="w-48 h-48" src="{img_url}" alt="Thumbnail">
+    {/if}
     <div class="textBox h-8 flex">
         <span>{path.split("\\").pop()}</span>
     </div>
