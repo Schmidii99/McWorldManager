f21ba9a8a624a0f8820b0098c96b35220034a684
Florian Schmid <Melonencool@gmail.com>
2024-07-12 10:30:41 +0200

    fix base64 utf8

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,17 +2,17 @@
     import WorldDisplay from '$lib/WorldDisplay.svelte';
     import { invoke } from '@tauri-apps/api/tauri';
     import { onMount } from 'svelte';
+    import { atobUTF8 } from '$lib/helper';
 
     let world_paths: string[] = [];
 
     onMount(async () => {
         let response: string = await invoke('get_saved_paths');
         // remove last empty item
-        console.log(response);
         world_paths = response.split(',');
         
         for (let i = 0; i < world_paths.length; i++) {
-            world_paths[i] = atob(world_paths[i]);
+            world_paths[i] = atobUTF8(world_paths[i]);
         }
     })
 </script>
